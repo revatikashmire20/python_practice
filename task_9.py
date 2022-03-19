d9bfa80d2e6e518272cc5ac7ef404269650fb836
revatikashmire20 <71587231+revatikashmire20@users.noreply.github.com>
2022-03-19 20:10:17 +0530

    Add files via upload

---
+++ task_9.py
@@ -0,0 +1,17 @@
+#task-9
+#Ask 2 numbers from users and store it in num1 and num2
+
+num1 = int(input("enter the num1 :"))
+num2 = int(input("enter the num2 :"))
+
+add = num1+num2
+print("addition =",add)
+
+sub = num1-num2
+print("substraction =",sub)
+
+mul = num1*num2
+print("multiplication =",mul)
+
+div = num1/num2
+print("division =",div)
